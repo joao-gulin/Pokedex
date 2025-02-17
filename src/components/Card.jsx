fdc8dd9ade3592aa421bce203f98de89a35efde8
joao-gulin <joaogulin@gmail.com>
2025-02-17 19:36:58 +0000

    cards styling finished

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,8 @@
 export default function Card(props) {
   return (
-    <div className="cards">
-      <div className="card">
-        <h1>{props.name}</h1>
-
-        <img src={props.imageUrl} alt="pokemon image" />
-
-      </div>
+    <div className="card">
+      <h1>{props.name}</h1>
+      <img src={props.imageUrl} alt="pokemon image" />
     </div>
-  )
+  );
 }
--- src/routes/Main.jsx
+++ src/routes/Main.jsx
@@ -1,28 +1,25 @@
-import { useState, useEffect } from "react"
-import Card from "../components/Card"
+import { useState, useEffect } from "react";
+import Card from "../components/Card";
 
 export default function Main() {
-  // Storing pokemon Data on react states
   const [pokemonData, setPokemonData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const apiURL = 'https://pokeapi.co/api/v2/pokemon?limit=151';
+  const apiURL = "https://pokeapi.co/api/v2/pokemon?limit=151";
 
-  // function to fetch the pokeapi data
   function fetchPokemons() {
     const fetchPokemonList = async () => {
       try {
         const response = await fetch(apiURL);
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error("Network response was not ok");
         }
         const data = await response.json();
         const pokemonDetails = await Promise.all(
           data.results.map(async (pokemon) => {
-            // Fetching details from the pokemon(name, front image)
             const detailsResponse = await fetch(pokemon.url);
             if (!detailsResponse.ok) {
-              throw new Error('Network response was not ok');
+              throw new Error("Network response was not ok");
             }
             const detailsData = await detailsResponse.json();
             return {
@@ -43,7 +40,7 @@ export default function Main() {
   }
 
   useEffect(() => {
-    fetchPokemons()
+    fetchPokemons();
   }, []);
 
   if (loading) {
@@ -57,13 +54,11 @@ export default function Main() {
   return (
     <main>
       <h1>Pokedex</h1>
-      {pokemonData.map((pokemon, i) => (
-        <Card
-          key={i}
-          imageUrl={pokemon.sprite}
-          name={pokemon.name}
-        />
-      ))}
+      <div className="cards">
+        {pokemonData.map((pokemon, i) => (
+          <Card key={i} imageUrl={pokemon.sprite} name={pokemon.name} />
+        ))}
+      </div>
     </main>
-  )
+  );
 }
