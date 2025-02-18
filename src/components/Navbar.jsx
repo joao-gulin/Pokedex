0a6b90e0b03cc392776daff14c1941b495358dd0
joao-gulin <joaogulin@gmail.com>
2025-02-18 22:43:30 +0000

    navbar home navigate implemented

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,19 @@
 import "../styles/Navbar.css"
 import PokemonLogo from "../assets/5.svg"
+import { useNavigate } from "react-router-dom"
 
 export default function Navbar() {
+  const navigate = useNavigate();
+
+  const handleGoHome = () => {
+    navigate("/")
+  }
+
   return (
     <header>
       <nav>
         <img src={PokemonLogo} alt="PokemonLogo" />
-        <span>Pokedex</span>
+        <span onClick={handleGoHome}>Pokedex</span>
       </nav>
     </header>
   )
